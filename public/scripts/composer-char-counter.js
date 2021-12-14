d08173c05a16a234a55b2d2fa469620d53dd998d
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-14 22:17:52 +0000

    Add event listener to tweet textarea input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,22 @@
 $(document).ready(() => {
 
-console.log('Composer connected');
+  console.log('Composer connected');
+  $("#tweet-text").on('keypress', () => {
+    console.log('keypress');
+  });
+  $("#tweet-text").on('keydown', () => {
+    console.log('keydown');
+  })
+  $("#tweet-text").on('keyup', () => {
+    console.log('keyup');
+  })
+  $("#tweet-text").on('blur', () => {
+    console.log('blur');
+  })
+  $("#tweet-text").on('change', () => {
+    console.log('change');
+  })
+  $("#tweet-text").on('input', () => {
+    console.log('input');
+  })
 });
