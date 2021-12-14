61a234d973560e0aa6e855cf9c60a2a7afee8cc3
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-14 22:56:42 +0000

    Add now grabs the value and length of input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,29 @@
 $(document).ready(() => {
 
-  console.log('Composer connected');
-  $("#tweet-text").on('keypress', () => {
-    console.log('keypress');
-  });
-  $("#tweet-text").on('keydown', () => {
-    console.log('keydown');
-  })
-  $("#tweet-text").on('keyup', () => {
-    console.log('keyup');
-  })
-  $("#tweet-text").on('blur', () => {
-    console.log('blur');
-  })
-  $("#tweet-text").on('change', () => {
-    console.log('change');
-  })
-  $("#tweet-text").on('input', () => {
-    console.log('input');
+  let counter = 141;
+  const tweetInput = $("#tweet-text");
+
+  // console.log('Composer connected');
+  // $("#tweet-text").on('keypress', () => {
+  //   console.log('keypress');
+  // });
+  // $("#tweet-text").on('keydown', () => {
+  //   console.log('keydown');
+  // })
+  // $("#tweet-text").on('keyup', () => {
+  //   console.log('keyup');
+  // })
+  // $("#tweet-text").on('blur', () => {
+  //   console.log('blur');
+  // })
+  // $("#tweet-text").on('change', () => {
+  //   const newTweet = tweetInput.val();
+  //   console.log('change', newTweet);
+  // })
+  $("#tweet-text").on('input', function () {
+    // const newTweet = this;
+    // const newTweet = tweetInput.val();
+    // console.log('input', newTweet, newTweet.length);
+    console.log('input', $(this).val(), $(this).val().length);
   })
 });
