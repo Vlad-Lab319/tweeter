520d33952b641572ec69b7e6b72fc6e83d028983
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-14 23:48:32 +0000

    Counter code cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,35 +1,8 @@
 $(document).ready(() => {
 
   const tweetLength = 140;
-  // const tweetInput = $("#tweet-text");
-  // console.log(tweetInput);
-  // $(".counter").text(tweetLength);
-
-  // console.log('Composer connected');
-  // $("#tweet-text").on('keypress', () => {
-  //   console.log('keypress');
-  // });
-  // $("#tweet-text").on('keydown', () => {
-  //   console.log('keydown');
-  // })
-  // $("#tweet-text").on('keyup', () => {
-  //   console.log('keyup');
-  // })
-  // $("#tweet-text").on('blur', () => {
-  //   console.log('blur');
-  // })
-  // $("#tweet-text").on('change', () => {
-  //   const newTweet = tweetInput.val();
-  //   console.log('change', newTweet);
-  // })
   $("#tweet-text").on('input', function () {
-    // const newTweet = this;
-    // const newTweet = tweetInput.val();
-    // console.log('input', newTweet, newTweet.length);
-    // console.log('input', $(this).val());
     let counter = tweetLength - $(this).val().length;
-    // console.log('parent', $(this).parent().prop("tagName"));
-    // $(".counter").text(counter);
     $($(this).parent().prop("tagName") + " div .counter").text(counter);
   })
 });
