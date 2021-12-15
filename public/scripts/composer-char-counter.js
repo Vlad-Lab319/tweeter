e83cf60603e362d4a70ff9fcb33cda49725c8e62
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-15 00:04:35 +0000

    Counter refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,12 +4,8 @@ $(document).ready(() => {
   $("#tweet-text").on('input', function () {
     let counter = tweetLength - $(this).val().length;
     const counterTag = $(this).parent().prop("tagName") + " div .counter";
-    $($(this).parent().prop("tagName") + " div .counter").text(counter);
-    if (counter < 0) {
-
-      $(counterTag).addClass("red");
-    } else {
-      $(counterTag).removeClass("red");
-    }
+    $(counterTag).text(counter);
+    (counter < 0) ? $(counterTag).addClass("red") : $(counterTag).removeClass("red");
+    
   })
 });
