8ff98df9d7745f22f95f61aeac8d1b1ef40003d3
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-15 00:00:02 +0000

    Add class red for the counter applied

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,13 @@ $(document).ready(() => {
   const tweetLength = 140;
   $("#tweet-text").on('input', function () {
     let counter = tweetLength - $(this).val().length;
+    const counterTag = $(this).parent().prop("tagName") + " div .counter";
     $($(this).parent().prop("tagName") + " div .counter").text(counter);
+    if (counter < 0) {
+
+      $(counterTag).addClass("red");
+    } else {
+      $(counterTag).removeClass("red");
+    }
   })
 });
