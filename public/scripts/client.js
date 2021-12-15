396faf11beb9a6003cc3e985387072da3d234ec8
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-15 06:02:43 +0000

    Add submit event listener and prevent default

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -48,31 +48,6 @@ const createTweetElement = function(data) {
 
 }
 
-
-// Test driver
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// const $tweet = createTweetElement(tweetData);
-
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
-// Fake data taken from initial-tweets.json
 const data = [
   {
     "user": {
@@ -99,5 +74,12 @@ const data = [
 ]
 
 renderTweets(data);
+
+
+$("#tweet-form").on("submit", function(event) {
+  event.preventDefault();
+  console.log('default prevented');
+});
+
 
 });
